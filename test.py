d9b89819cb7f86094439bc48932d97d487e86817
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-09-06 00:28:19 +0900

    add忘れ

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from webtest import TestApp
+from main import application
+from nose.tools import *
+
+app = TestApp(application())
+
+def test_index():
+  response = app.get('/')
+  ok_(True)
+  #assert 'Hello world!' in str(response)
+
